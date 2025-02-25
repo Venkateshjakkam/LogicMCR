696b504ef69716393a1beb084008c33d6f8aa0d2
vidyaprasadam1999 <vidyaprasadam1999@gmail.com>
2025-02-25 18:17:12 +0530

    uniqeCommit

--- PracticeJs/index.js
+++ PracticeJs/index.js
@@ -202,18 +202,195 @@
 
 /////////////////------------  ClearAllTimeOut-------------Example----1-----////////////////
 
-let timer = 0;
+// let timer = 0;
 
-function incrTime() {
-    timer++;
-    console.log("Timer:", timer);
+// function incrTime() {
+//     timer++;
+//     console.log("Timer:", timer);
 
-    if (timer === 20) {
-        // console.log("Clearing timeout simulation.");
-        return () => clearTimeout(timer);
+//     if (timer === 20) {
+//         return () => clearTimeout(timer);
+//     }
+
+//     setTimeout(incrTime, 1000); 
+// }
+
+// incrTime();
+
+
+///////////////////////////////-------- Example -----------------2///////////////////////////////
+
+// (function () {
+   
+//     const originalSetTimeout = window.setTimeout;
+//     const originalClearTimeout = window.clearTimeout;
+
+//     let timeoutIds = [];
+
+//     window.setTimeout = function (callback, delay) {
+//         let timeoutId = originalSetTimeout(callback, delay);
+//         timeoutIds.push(timeoutId); 
+//         return timeoutId;
+//     };
+
+//     window.clearTimeout = function (timeoutId) {
+//         timeoutIds = timeoutIds.filter(id => id !== timeoutId); 
+//         originalClearTimeout(timeoutId);
+//     };
+
+//     window.clearAllTimeout = function () {
+//         timeoutIds.forEach(id => originalClearTimeout(id)); 
+//         timeoutIds = []; 
+//     };
+//     console.log(timeoutIds);
+// })();
+
+// setTimeout(() => console.log("Function 1 executed"), 1000);
+// setTimeout(() => console.log("Function 2 executed"), 1000);
+// setTimeout(() => console.log("Function 3 executed"), 1000);
+
+// clearAllTimeout();
+
+/////////////////////////----------------------- Intersection of Unique element in the array-------------------////////////////////
+
+
+// let arr1 = ['ab','bc','ad','ae','ac','ad','ae','mn'];
+// let arr2 = ['na','ba','ab','mn','jk','ba','mn','na'];
+
+// function uniqEle(arr1,arr2){
+
+//    let set1 = [];
+//    let set2 = [];
+
+//    for(let i=0; i<=arr1.length-1; i++){
+//     if(set1.includes(arr1[i]) === false){
+//         set1.push(arr1[i])
+//     }
+//    }
+    
+
+//    for(let i=0; i<=arr2.length-1; i++){
+//     if(set2.includes(arr2[i]) === false){
+//         set2.push(arr2[i])
+//     }
+//    }
+//    console.log(set1,set2)
+
+//     let arr3 =[]
+
+//     for(let i=0; i<= set1.length-1; i++){
+//         for(let j=0; j<= set2.length-1; j++){
+//            if( set1[i] === set2[j]){
+//             arr3.push(set1[i])
+//            }
+//         }
+//     }
+
+//     return arr3.toString();
+
+// }
+// console.log(uniqEle(arr1,arr2));
+
+
+///////// Remove characters ------------------- Example-1-------------///////////////////
+
+
+// function charDel(char){
+
+//     let newChar = char.split('');
+//     console.log(newChar);
+
+//     let outPut = []
+//     for(let i=0; i<=newChar.length-1; i++){
+//         if(newChar[i] === 'c' && newChar.length >0 && newChar[newChar.length-1] === 'a' ){
+//              outPut.pop(); 
+//              outPut.push(newChar[i]);
+//              continue;
+//         }
+//     }
+
+
+//     return outPut.join('').toString();
+// }
+
+// console.log(charDel('ab'));
+// console.log(charDel('abababaaaabbccdddba'));
+// console.log(charDel('cabbaabcca') )
+
+
+
+// ///////////////////////////////////------Binary-Unique-elements in the array-----///////////////////////////////
+// let arr1 = [1,1,3,4,5,3,7,8,2,5,6,7];
+
+// function uniqArr(arr1){
+//     let outPut = [];
+//         for(let i=0; i<=arr1.length-1; i++){
+//             for(let j=i+1; j<arr1.length; j++){
+//                 if(arr1[i]< arr1[j]){
+//                     temp = arr1[i];
+//                     arr1[i] = arr1[j];
+//                     arr1[j] = temp
+//                 }
+//             } 
+//         }
+
+//         for(let i=0; i <= arr1.length-1; i++){
+//             if(arr1[i] !== arr1[i+1]){
+//                 outPut.push(arr1[i])
+//             }
+//         }
+//         return outPut;
+// }
+// console.log(uniqArr(arr1));
+
+//////////////Sorting an aray----------------Exaple-1------------////////////////////
+
+
+
+// let arr1 = [2,8,4,5,7,3,1,6]
+
+// let outPut = arr1.sort();
+// console.log(arr1);
+
+
+// function sortArr(arr1){
+//     for(let i=0; i <= arr1.length-1; i++ ){
+//         for(j=i+1; j<arr1.length; j++){
+//             if(arr1[i]<arr1[j]){
+//                 temp = arr1[i];
+//                 arr1[i] = arr1[j];
+//                 arr1[j] = temp;
+//             }
+//         }
+//     }
+//     return arr1;
+// }
+// console.log(sortArr(arr1));
+
+
+
+/////////////////////------------------ BinarySearch---------------//////
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        if (arr[mid] === target) {
+            return mid; 
+        } else if (arr[mid] < target) {
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
     }
-
-    setTimeout(incrTime, 1000); // Call itself every second
+    
+    return -1; 
 }
 
-incrTime();
+
+let arr = [1, 3, 5, 7, 9, 11];
+console.log(binarySearch(arr, 5)); 
+console.log(binarySearch(arr, 6)); 
